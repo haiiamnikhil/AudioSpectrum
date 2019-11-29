b63e6bedf8a0a1336711ab26ba7ac0f737f9be33
haiiamnikhil <haiiamnikhil@gmail.com>
2019-11-29 22:24:06 +0530

    master

--- Record.py
+++ Record.py
@@ -1,6 +1,8 @@
 import sounddevice as sd
 from scipy.io import wavfile
 import os as os
+import datetime
+import time
 
 try:
     os.mkdir("Recordings")
@@ -11,7 +13,13 @@ FramesperSec = 44100
 RecDur = 1
 count = 0
 recount = 0
-while count <= 500:
+startTime = datetime.datetime(2019,11,29,22,25)
+stopTime = datetime.datetime(2019,11,29,22,28)
+
+while datetime.datetime.now() < startTime:
+    time.sleep(1)
+
+while datetime.datetime.now >= startTime and datetime.datetime.now() <= stopTime:
     recordAudio = sd.rec((RecDur * FramesperSec),samplerate=FramesperSec,channels=2)
     sd.wait()
     wavfile.write(os.path.join("Recordings",f"recorded {recount}.wav"),FramesperSec,recordAudio)
